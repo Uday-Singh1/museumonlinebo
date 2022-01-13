2d727d5a7fcb57c45dac14191f7f12bc440c9db0
Uday singh 1 <91188742+Uday-Singh1@users.noreply.github.com>
2022-01-13 21:51:01 +0100

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -5,3 +5,24 @@ function on() {
   function off() {
     document.getElementById("blackopacity").style.display = "none";
   }
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
